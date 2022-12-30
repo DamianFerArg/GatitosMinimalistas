51437fc59889b312f886579e3265fb680047651f
Damian <damianfer1304@gmail.com>
2022-12-30 02:04:21 -0300

    OptimizacionJS

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-let edad=Number (prompt("Ingrese su edad"))
-
-if (edad>=18){
-    alert("Usted puede continuar")
-} else{
-    alert("Menores de edad no estan avalados por la BCRA")
-    
+do {
+    edad = prompt('Ingrese su edad');
+    if (edad >= 18) {
+        alert('Usted puede continuar');
+    } else {
+        alert('Prohibido el ingreso para menores de edad');
+    }
 }
+while (edad < 18)
+
+const tarjetaBarata = 200
+const tarjetaNoTanBarata = 500
+const tarjetaPipicucu = 900
+
+let Nombre = prompt("Como es su nombre?");
+
+do {
+    gasto = prompt(Nombre + ", cuanto desea gastar para su tarjeta?");
+    if (gasto >= tarjetaPipicucu) {
+        alert("Le recomendamos el plan Pipícucú");
+    }
+
+    else if (gasto >= tarjetaNoTanBarata) {
+        alert("Le recomendamos el plan no tan barato");
+    }
+
+    else if (gasto >= tarjetaBarata) {
+        alert("Le recomendamos el plan barato");
+    }
+
+    else if (gasto < tarjetaBarata) {
+        alert("No cubre el costo de nuestros planes");
+    }
+
+} while (gasto < 300)
